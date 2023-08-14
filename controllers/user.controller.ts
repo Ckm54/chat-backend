4504234cbe7aab7dfb2257906f72ef2a31be9b67
Ckm54 <mutaic84@gmail.com>
2023-08-14 18:44:55 +0300

    :hammer: add route to login user

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -6,6 +6,12 @@ export async function registerUser(req: Request, res: Response) {
   try {
     const { username, email, password, fullName } = req.body;
 
+    if (!username || !email || !password)
+      return res.status(400).json({
+        errorCode: "INCOMPLETE_FIELDS",
+        message: "Please ensure yo have entered a username, email and password",
+      });
+
     // check if there is an existing user with this name
     const usernameExists = await User.findOne({ username });
 
@@ -38,6 +44,46 @@ export async function registerUser(req: Request, res: Response) {
     await newUser.save();
 
     return res.status(201).json({ message: "User created successfully" });
+  } catch (error: any) {
+    console.error("Error registering user:", error);
+    res.status(500).json({
+      errorCode: "INTERNAL_ERROR",
+      message: "An error occurred while registering the user" + error.message,
+    });
+  }
+}
+
+export async function loginUser(req: Request, res: Response) {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password)
+      return res
+        .status(400)
+        .json({ message: "username and/or password missing" });
+
+    // try fetching user from database by username given
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      // no matching user found in db
+      return res
+        .status(400)
+        .json({ message: "Incorrect username and/or password" });
+    }
+
+    // user exists so continue to check password validity
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid)
+      return res
+        .status(401)
+        .json({ message: "Incorrect username and/or password" });
+
+    const { password: passwordHash, ...userWithoutPassword } = user.toJSON();
+
+    // login success!
+    return res.status(200).json({ userWithoutPassword });
   } catch (error: any) {
     console.error("Error registering user:", error);
     res.status(500).json({
--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { registerUser } from "../controllers/user.controller";
+import { loginUser, registerUser } from "../controllers/user.controller";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
